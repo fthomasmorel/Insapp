4c38b86924f87335ff4f614fd0877cabbf77c04f
fthomasmorel <florent@thomasmorel.io>
2017-01-24 16:48:11 +0100

    update search route

--- Insapp-go/src/searchController.go
+++ Insapp-go/src/searchController.go
@@ -5,38 +5,52 @@ import (
 	"net/http"
 
 	"gopkg.in/mgo.v2/bson"
-  "github.com/gorilla/mux"
 )
 
+type Search struct {
+	Terms       string          `json:"terms"`
+}
+
 func SearchUserController(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	users := SearchUser(vars["name"])
+    decoder := json.NewDecoder(r.Body)
+	var search Search
+	decoder.Decode(&search)
+    users := SearchUser(search.Terms)
 	json.NewEncoder(w).Encode(bson.M{"users": users})
 }
 
 func SearchPostController(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	posts := SearchPost(vars["name"])
+    decoder := json.NewDecoder(r.Body)
+	var search Search
+	decoder.Decode(&search)
+    posts := SearchPost(search.Terms)
 	json.NewEncoder(w).Encode(bson.M{"posts": posts})
 }
 
 func SearchEventController(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	events := SearchEvent(vars["name"])
+    decoder := json.NewDecoder(r.Body)
+	var search Search
+	decoder.Decode(&search)
+	events := SearchEvent(search.Terms)
 	json.NewEncoder(w).Encode(bson.M{"events": events})
 }
 
 func SearchAssociationController(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	assos := SearchAssociation(vars["name"])
+    decoder := json.NewDecoder(r.Body)
+	var search Search
+	decoder.Decode(&search)
+	assos := SearchAssociation(search.Terms)
 	json.NewEncoder(w).Encode(bson.M{"associations": assos})
 }
 
 func SearchUniversalController(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-  users := SearchUser(vars["name"])[0:30]
-  posts := SearchPost(vars["name"])[0:30]
-  events := SearchEvent(vars["name"])[0:30]
-	assos := SearchAssociation(vars["name"])[0:30]
+    decoder := json.NewDecoder(r.Body)
+	var search Search
+	decoder.Decode(&search)
+
+    users := SearchUser(search.Terms)[0:30]
+    posts := SearchPost(search.Terms)[0:30]
+    events := SearchEvent(search.Terms)[0:30]
+	assos := SearchAssociation(search.Terms)[0:30]
 	json.NewEncoder(w).Encode(bson.M{"associations": assos, "users": users, "posts": posts, "events": events})
 }
