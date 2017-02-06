79ffaceb35ed6fd4f62e4ed9ab08758787744836
fthomasmorel <florent@thomasmorel.io>
2017-02-06 08:58:10 +0100

    update gitignore

--- Insapp-web/app/app.config.js
+++ Insapp-web/app/app.config.js
@@ -1,7 +1,7 @@
 var app = angular.module('insapp', ['ngRoute','ngResource','ui.bootstrap.datetimepicker', 'ngFileUpload', 'ngDialog', 'ngFileUpload', 'ngLoadingOverlay','angular-spinkit']);
 
 app.constant('configuration', {
-  api: 'https://dev.insapp.fr/api/v1',
-  cdn: 'https://dev.insapp.fr/cdn/',
+  api: 'https://insapp.fr/api/v1',
+  cdn: 'https://insapp.fr/cdn/',
   baseUrl: '/web',
 });
